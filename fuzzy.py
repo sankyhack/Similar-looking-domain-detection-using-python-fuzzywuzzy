712a786f7ad182269a9730989f71d09f210bf039
sankyhack <68192011+sankyhack@users.noreply.github.com>
2021-11-05 21:38:35 +0530

    Update fuzzy.py

--- fuzzy.py
+++ fuzzy.py
@@ -4,7 +4,7 @@ dns_data = open(r'/home/user/Desktop/BEC/your_domain.txt','r') # List of genuine
 output = open(r'/home/user/Desktop/BEC/output.txt','w')		# Output file (change path as per your directory structure)
 
 for dns in dns_data:    
-    domain = open(r'/home/user/Desktop/BEC/domain-names-data.txt','r') # domain data gathered from proxy/dns/mail logs (change path as per your directory structure)
+    domain = open(r'/home/user/Desktop/BEC/domain-names-data.txt','r') # domains related data gathered from proxy/dns/mail logs (SIEM will help you) (change path as per your directory structure)
     for site in domain:
         if ( fuzz.ratio(site.rstrip(),dns.rstrip()) > 80 ):
             output.write( dns.rstrip() + "\t" + site.rstrip() + "\t" + str(fuzz.ratio(site.rstrip(),dns.rstrip())))
